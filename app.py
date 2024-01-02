25aded7deddda9679663b8ab76270f7d0402f497
vivioliver <122638547+vivioliver@users.noreply.github.com>
2024-01-02 00:31:50 -0300

    flask

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -6,12 +8,32 @@ app = Flask(__name__)
 # Configurações do banco de dados
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'labinfo'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'AtemporalBrand'
 
 # Inicialização do MySQL
 mysql = MySQL()
 mysql.init_app(app)
+
+usuarios = {
+    'cliente1': 'cliente@gmail.com1'
+}
+
+@app.route('/')
+def inicial():
+    return render_template('principal.html')
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/enviar', methods=['POST'])
+def enviar():
+    return render_template('cadastro.html')
+    c = request.form['usuario']
+    m = request.form['mens']
+    usuarios[c] = m
+    return render_template('resposta.html', usuario=usuarios)
 
 @app.route('/Produto')
 def produtos():
