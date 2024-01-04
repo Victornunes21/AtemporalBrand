be7402778b6d57cd74babefb346936cd171d2990
vivioliver <122638547+vivioliver@users.noreply.github.com>
2024-01-04 11:33:19 -0300

    flask

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Users\20201174010005\Documents\GitHub\AtemporalBrand
+set VIRTUAL_ENV=C:\Users\15324720488\Documents\GitHub\AtemporalBrand
 
 if not defined PROMPT set PROMPT=$P$G
 
